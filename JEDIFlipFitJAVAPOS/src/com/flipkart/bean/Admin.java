0d709ef5a2ec0ac7eec440e06e67ed44fa33ceca
FkYeshwanth <kondur.s@flipkart.com>
2024-01-24 00:25:30 +0530

    second commit

--- JEDIFlipFitJAVAPOS/src/com/flipkart/bean/Admin.java
+++ JEDIFlipFitJAVAPOS/src/com/flipkart/bean/Admin.java
@@ -2,10 +2,34 @@
  * 
  */
 package com.flipkart.bean;
+public class Admin extends Person {
+    private String name;
+    private String phoneNumber;
 
-/**
- * 
- */
-public class Admin {
+    public Admin(String email, String password, String roleName, String name, String phoneNumber) {
+        super(email, password, roleName);
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
 
+    public Admin() {
+        super();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
+
--- JEDIFlipFitJAVAPOS/src/com/flipkart/bean/Person.java
+++ JEDIFlipFitJAVAPOS/src/com/flipkart/bean/Person.java
@@ -1,5 +1,45 @@
 package com.flipkart.bean;
 
+/**
+ *
+ */
 public class Person {
+    private String email;
+    private String password;
+    private String roleId;
 
+    public Person(String email, String password, String roleId) {
+        this.email = email;
+        this.password = password;
+        this.roleId = roleId;
+    }
+
+    public Person() {
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
 }
+
