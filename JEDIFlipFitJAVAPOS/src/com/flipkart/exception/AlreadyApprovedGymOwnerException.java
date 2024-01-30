3d53a54a3e688d1829db48df155fce5f8a58977c
saketm2707 <saketmundra2707@gmail.com>
2024-01-31 00:58:37 +0530

    Exceptions Added

---
+++ JEDIFlipFitJAVAPOS/src/com/flipkart/exception/AlreadyApprovedGymOwnerException.java
@@ -0,0 +1,8 @@
+package com.flipkart.exception;
+
+public class AlreadyApprovedGymOwnerException extends Exception {
+    public AlreadyApprovedGymOwnerException(String message){
+        super(message);
+    }
+}
+
